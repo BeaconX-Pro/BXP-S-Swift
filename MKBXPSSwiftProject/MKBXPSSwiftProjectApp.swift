fcc952e2a6012b0ef449a38fd41069dd856b3bab
lovexiaoxia <aadyx2007@163.com>
2026-09-12 11:14:41 +0800

    Initial Commit

---
+++ MKBXPSSwiftProject/MKBXPSSwiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MKBXPSSwiftProjectApp.swift
+//  MKBXPSSwiftProject
+//
+//  Created by aa on 2026/9/12.
+//
+
+import SwiftUI
+
+@main
+struct MKBXPSSwiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
